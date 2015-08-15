59cd2a6c810559af1f8c1e90639b4af64caba317
Vuk Jovanovic <southpolenator@gmail.com>
2015-08-15 13:16:27 +0200

    Adding configuration for GameCrawler

---
+++ GameCrawler/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace GameCrawler
+{
+    class Configuration
+    {
+        public string FicsUsername { get; set; } = "guest";
+        public string FicsPassword { get; set; } = "";
+        public string DbServer { get; set; } = "";
+        public string DbName { get; set; } = "";
+        public string DbUsername { get; set; } = "";
+        public string DbPassword { get; set; } = "";
+    }
+}
