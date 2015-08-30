a31d99b33d039ced257cc8cb825ef08c31f769bd
Vuk Jovanovic <southpolenator@gmail.com>
2015-08-30 11:32:35 +0200

    Adding configuration to CrawledGamesWebRole

---
+++ CrawledGamesWebRole/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace CrawledGamesWebRole
+{
+    class Configuration
+    {
+        public string DbServer { get; set; } = "<dbserver>";
+        public string DbName { get; set; } = "<dbname>";
+        public string DbUsername { get; set; } = "<username>";
+        public string DbPassword { get; set; } = "<password>";
+    }
+}
